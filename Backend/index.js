19c4e757de2e8dbf7a95d0dc38da3d259a2602f9
Sandhiya1903 <sandhiyaelumalai19@gmail.com>
2024-10-21 21:20:25 +0530

    setup

---
+++ Backend/index.js
@@ -0,0 +1,33 @@
+//packages
+
+import express from "express";//A framework for building web applications and APIs.
+import cookieParser from "cookie-parser";//Middleware to parse cookies from incoming requests.
+import dotenv from "dotenv"; //A package to load environment variables from a .env file.
+import path from "path";//A core Node.js module used for handling file paths.
+
+//files
+
+import connectDB from "./config/db.js";
+
+//configuration
+
+dotenv.config() 
+connectDB()
+
+const app = express()
+
+//middlewares
+
+app.use(express.json)
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser)
+
+const PORT =process.env.PORT || 3000 
+
+//Routes
+
+
+
+app.listen(PORT,()=>console.log(`server is running on port ${PORT}`))
+
+
